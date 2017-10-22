63e2959715e4b0ac2b0776c9d0988e358a1d3c1d
AdmiralDzzle <generallemur@gmail.com>
2017-10-21 22:37:48 -0400

    Added initial C++ files

---
+++ Source/BirthdayLasers/visionCone.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "visionCone.h"
+
+visionCone::visionCone()
+{
+}
+
+visionCone::~visionCone()
+{
+}
+//test
